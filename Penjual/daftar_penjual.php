c70500361622add5f9fcff146da3949f81c792d7
evavo19 <124240156@student.upnyk.ac.id>
2026-04-24 07:09:36 +0700

    penjual done bismillah, yg tadi salah ketik 'pesanan done2' haha

--- Penjual/daftar_penjual.php
+++ Penjual/daftar_penjual.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Daftar Penjual - Shopee</title>
+    <title>Daftar Penjual - Penjual</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <script src="https://unpkg.com/lucide@latest"></script>
--- Penjual/penjual.php
+++ Penjual/penjual.php
@@ -34,7 +34,7 @@ foreach ($kata as $k) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profil - Penjual</title>
+    <title>Profil Penjual - Penjual</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600;700&family=Poppins:wght@400;700&family=Roboto:wght@400;500&display=swap" rel="stylesheet">
     <script src="https://unpkg.com/lucide@latest"></script>

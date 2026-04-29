73f0e18f3d042e0dd039d23592681ccb369a9e9d
evavo19 <124240156@student.upnyk.ac.id>
2026-04-29 22:45:47 +0700

    driver middle mile infinity

--- Driver_Middle_Mile/status_pengiriman_driver.php
+++ Driver_Middle_Mile/status_pengiriman_driver.php
@@ -135,9 +135,9 @@ $deliveryData = [
                 <button class="w-full bg-brand hover:brightness-110 text-white font-bold py-4 rounded-2xl transition shadow-lg shadow-orange-100 active:scale-[0.98]">
                     Gunakan Pintasan Rute
                 </button>
-                <button class="w-full bg-zinc-900 hover:bg-black text-white font-bold py-4 rounded-2xl transition shadow-lg shadow-gray-200 active:scale-[0.98]">
+                <a href="/Operator_Last_Mile/index_operator.php" class="w-full bg-zinc-900 hover:bg-black text-white font-bold py-4 rounded-2xl transition shadow-lg shadow-gray-200 active:scale-[0.98] flex items-center justify-center">
                     Konfirmasi Tiba di Hub
-                </button>
+                </a>
             </div>
         </div>
 

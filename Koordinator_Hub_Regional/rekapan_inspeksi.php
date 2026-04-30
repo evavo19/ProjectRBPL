2abaeae0bcb982554ed16f331aa29a56318f6ad2
evavo19 <124240156@student.upnyk.ac.id>
2026-04-30 14:23:16 +0700

    perubahan koordinator hub regional

--- Koordinator_Hub_Regional/rekapan_inspeksi.php
+++ Koordinator_Hub_Regional/rekapan_inspeksi.php
@@ -1,8 +1,8 @@
 <?php
-$host = "sql203.infinityfree.com";
-$user = "if0_41736846";
-$password = "tugasRBPL2026";
-$database = "if0_41736846_db_rbpl";
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "rbpl";
 
 $conn = mysqli_connect($host, $user, $password, $database);
 
